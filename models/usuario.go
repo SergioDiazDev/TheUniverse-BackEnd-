26254b8f29ae8fc0e18323dccc2aed6a887d8756
SergioDiazRuiz <55483457+SergioDiazRuiz@users.noreply.github.com>
2022-03-29 19:58:49 +0200

    otro mas 2

--- models/usuario.go
+++ models/usuario.go
@@ -8,15 +8,15 @@ import (
 
 //Usuario modelo usuario de la BD
 type Usuario struct {
-	ID              primitive.ObjectID `bson:"_id, omitempty" json:"id"`
+	ID              primitive.ObjectID `bson:"_id, omitempty" json:"_id"`
 	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
 	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
 	FechaRegistro   time.Time          `bson:"fechaRegistro" json:"fechaRegistro,omitempty"`
 	Correo          string             `bson:"correo" json:"correo"`
 	Pass            string             `bson:"contraseña" json:"contraseña,omitempty"`
-	//Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
-	//Banner          string             `bson:"banner" json:"banner,omitempty"`
-	//Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
-	//Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
-	//SitioWeb        string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner          string             `bson:"banner" json:"banner,omitempty"`
+	Biografia       string             `bson:"biografia" json:"biografia,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+	SitioWeb        string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
 }
